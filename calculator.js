026f51b1e9c309fd7bc214eae4dd44e18dd3bb07
dividp <156876160+dividp@users.noreply.github.com>
2024-01-19 17:06:57 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,35 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Wait for the DOM to be fully loaded before attaching event listeners
-    attachEventListeners();
-});
-
-function attachEventListeners() {
-    // Get the calculator buttons
-    var buttons = document.querySelectorAll('.buttons button');
-
-    // Attach click event listener to each button
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            handleButtonClick(button.innerText);
-        });
-    });
-}
-
-function handleButtonClick(value) {
-    var display = document.getElementById('display');
-
-    switch (value) {
-        case '=':
-            calculateResult();
-            break;
-        case 'C':
-            clearDisplay();
-            break;
-        default:
-            appendToDisplay(value);
-    }
-}
-
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
